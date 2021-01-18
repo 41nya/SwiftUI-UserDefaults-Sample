173dbf36f4fe0775225ebf33d8e3d82eb1c739b6
41nya <sny.cho@gmail.com>
2021-01-18 10:53:32 +0900

    Initial Commit

---
+++ SwiftUI-UserDefaults-Sample/SwiftUI_UserDefaults_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UserDefaults_SampleApp.swift
+//  SwiftUI-UserDefaults-Sample
+//
+//  Created by 41nya on 2021/01/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UserDefaults_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
